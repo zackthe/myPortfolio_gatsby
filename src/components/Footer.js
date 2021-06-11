7d3d1d4de720a19f9ef65552e5d7be1ffc63a071
Belkasmi Zakariae <48525328+zackthe@users.noreply.github.com>
2021-06-11 19:12:31 +0100

    footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,10 @@ import logo from "../assets/images/logo.png"
 import {useState , useEffect, useRef} from 'react'
 import { FaSmile } from "react-icons/fa"
 import { StaticImage } from "gatsby-plugin-image"
+import { FaGithub } from 'react-icons/fa';
+import { GrGatsbyjs} from 'react-icons/gr';
+import {  SiNetlify} from 'react-icons/si';
+
 
 
 
@@ -33,39 +37,42 @@ const Footer = () => {
 
 
 
-  return <footer className="footer">
-      <div className="footer-title-wraper">
-       <h1 className="footer-title">You got a project ? let's talk</h1>
-       <div className="alert-msg-container">
-    <p className={isCopied ? "alert-msg-show": "alert-msg"} ref={copytext}>email copied <FaSmile color = 'white'></FaSmile></p>
-       </div>
-       <button className="footer-btn btn" onClick={copyToClipboard}>Copy email</button>
-      
-       </div>
-       <div className="footer-navigation-wraper">
-       <Link to="/" aria-label='home page' ><div className="footer-logo"><StaticImage className="logo-img" src="../assets/images/logo.png" alt="logo" ></StaticImage></div></Link>
-       <nav className="footer-navigation">
-           <ul>
-              <li><Link to="/projects"><p>Projects</p></Link></li> 
-              <li><Link to="/about"><p>About</p></Link></li> 
-              <li><Link to="/blog/"><p>Blog</p></Link></li> 
-           </ul>
-       </nav>
-       <div className="footer-social-links">
-          <p>follow me on :</p>
-          <div className="social-links">
+    return <footer className="footer">
+            <div className="footer-title-wraper">
+               <h1 className="footer-title">You got a project ? let's talk</h1>
+                  <div className="alert-msg-container">
+                    <p className={isCopied ? "alert-msg-show": "alert-msg"} ref={copytext}>email copied <FaSmile color = 'white'></FaSmile></p>
+                 </div>
+            <button className="footer-btn btn" onClick={copyToClipboard}>Copy email</button>
+            
+            </div>
+            <div className="footer-navigation-wraper">
+            <Link to="/" aria-label='home page' ><div className="footer-logo"><StaticImage className="logo-img" src="../assets/images/logo.png" alt="logo" ></StaticImage></div></Link>
+            <nav className="footer-navigation">
+               <ul>
+                  <li><Link to="/projects"><p>Projects</p></Link></li> 
+                  <li><Link to="/about"><p>About</p></Link></li> 
+                  <li><Link to="/blog/"><p>Blog</p></Link></li> 
+               </ul>
+            </nav>
+            <div className="footer-social-links">
+               <p>follow me on :</p>
+               <div className="social-links">
 
-{            social.map(link =>{
-        const {id,icon,url} = link
-        return <a  href={url} target="_blank" rel="noreferrer" aria-label='social icons'>{icon}</a>
-})
-}
+      {            social.map(link =>{
+            const {id,icon,url} = link
+            return <a  href={url} target="_blank" rel="noreferrer" aria-label='social icons'>{icon}</a>
+      })
+      }
 
 
-          </div>
-       </div>
-       </div>
-    </footer>
+               </div>
+            </div>
+            </div>
+
+            <p style={{textAlign: 'center', fontSize : '1.3em'}}>This site is built using <GrGatsbyjs></GrGatsbyjs> open sourced on <FaGithub></FaGithub> deployed on <SiNetlify></SiNetlify></p>
+
+         </footer>
 }
 
 export default Footer
